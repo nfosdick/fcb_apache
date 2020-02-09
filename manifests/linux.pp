455cfe88c2ad895f2daff26b5bf9793179255b46
Nick Fosdick <nick@lark-it.com>
2020-02-09 11:50:25 -0700

    updates

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -10,17 +10,17 @@ class fcb_apache::linux(
   $vhosts.each |$vhost, $config| {
     $merged_nonssl_config = $vhosts_defaults['vhost_nonssl'] + $config
     $merged_ssl_config    = $vhosts_defaults['vhost_ssl'] + $config
-    notify{ "Blah $config":}
+
     apache::vhost { "${vhost}_nonssl":
       servername => $vhost,
       *          => $merged_nonssl_config,
     }
 
     if has_key($config, 'ssl') {
-#      apache::vhost { "${vhost}_ssl":
-#        servername => $vhost,
-#        *          => $merged_ssl_config,
-#      }  
+      apache::vhost { "${vhost}_ssl":
+        servername => $vhost,
+        *          => $merged_ssl_config,
+      }  
     }
   }
 }
