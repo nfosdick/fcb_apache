f655bd295cd7cf7e2e1754eee0438d9d73b101a2
Nick Fosdick <nick@lark-it.com>
2020-02-18 19:27:09 -0700

    updating

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,9 +1,10 @@
 class fcb_apache_v2::windows(
   $version          = '2.4.41',
   $architecture     = $facts['architecture'],
-  $httpd_zip        = "httpd-${version}-win${architecture}-VS16.zip",
-#  $url              = "http://www.apachelounge.com/download/VS16/binaries/${httpd_zip}",
-  $url              = "https://send.firefox.com/download/7c843ee7605febd9/#D0Vbjr9iBMDsfzsRRaLQ7g",
+  #$httpd_zip        = "httpd-${version}-win${architecture}-VS16.zip",
+  #$url              = "http://www.apachelounge.com/download/VS16/binaries/${httpd_zip}",
+  $httpd_zip        = "httpd-${version}-o111c-${architecture}-vc15-r2.zip",
+  $url              = "https://www.apachehaus.com/downloads/${httpd_zip}",
   $destination_path = 'c:/larktemp',
   $zipfile          = "${destination_path}/${httpd_zip}",
 ){
@@ -17,5 +18,6 @@ class fcb_apache_v2::windows(
     dsc_ensure      => 'present',
     dsc_path        => $zipfile,
     dsc_destination => 'c:/nick',
+    require         => Dsc_remotefile[ "Download ${httpd_zip}" ]
   }
 }
