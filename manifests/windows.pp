71b590daffd20e1d2d1cda0c89acd63b2012eb5e
Nick Fosdick <nick@lark-it.com>
2020-02-18 19:38:07 -0700

    updating

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,8 +1,6 @@
 class fcb_apache_v2::windows(
   $version          = '2.4.41',
   $architecture     = $facts['architecture'],
-  #$httpd_zip        = "httpd-${version}-win${architecture}-VS16.zip",
-  #$url              = "http://www.apachelounge.com/download/VS16/binaries/${httpd_zip}",
   $httpd_zip        = "httpd-${version}-o111c-${architecture}-vc15-r2.zip",
   $url              = "https://www.apachehaus.com/downloads/${httpd_zip}",
   $destination_path = 'c:/larktemp',
