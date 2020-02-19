9f57b9acead01628af2026ada757f623400f77a5
Nick Fosdick <nick@lark-it.com>
2020-02-18 18:43:47 -0700

    updating

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -6,9 +6,10 @@ class fcb_apache_v2::windows(
   $destination_path = 'c:/larktemp',
 ){
   notify{"Nick  $url, ${destination_path}/${httpd_zip}":}
-#  dsc_xremotefile {"Download ${httpd_zip}":
-#    dsc_destinationpath => "${destination_path}/${httpd_zip}"",
-#    dsc_uri             => $url,
-#  }
+  dsc_xremotefile {"Download ${httpd_zip}":
+    dsc_destinationpath => "${destination_path}/${httpd_zip}",
+#    dsc_destinationpath => 'c:/larktemp/httpd-2.4.41-winx64-VS16.zip',
+    dsc_uri             => $url,
+  }
   #https://www.apachelounge.com/download/VS16/binaries/httpd-2.4.41-win64-VS16.zip
 }
