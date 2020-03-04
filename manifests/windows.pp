6b3709b9ba6d79246ebd26f8971ee20d8b965092
Nick Fosdick <nick@lark-it.com>
2020-03-03 19:10:49 -0700

    test

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -5,6 +5,7 @@ class fcb_apache_v2::windows(
   $url              = "https://www.apachehaus.com/downloads/${httpd_zip}",
   $destination_path = 'c:/larktemp',
   $zipfile          = "${destination_path}/${httpd_zip}",
+  $install_path     = "c:\"
 ){
 
   notify{"Nick $url":}
@@ -17,8 +18,16 @@ class fcb_apache_v2::windows(
   dsc_archive { "Unzip ${httpd_zip} and Copy the Content":
     dsc_ensure      => 'present',
     dsc_path        => $zipfile,
-    dsc_destination => 'c:/nick',
+    dsc_destination => $install_path,
 #    require         => Dsc_xremotefile[ "Download ${httpd_zip}" ],
+  }
+
+  exec { "Install tomcat-${version} Windows Service":
+    command   => "$service_cmd install tomcat-${version}",
+    unless    => "if(Get-Service tomcat-${version}){ exit 0 }else{ exit 1 }",
+    provider  => powershell,
+   #logoutput => true,
+    require   => Dsc_archive[ "Unzip $zip_file" ],
   }
 
 
