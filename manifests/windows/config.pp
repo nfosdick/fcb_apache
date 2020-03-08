1be8b48613f366bad6b383a9f86d374591800c8b
Nick Fosdick <nick@lark-it.com>
2020-03-08 17:23:02 -0600

    Adding install.pp

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -7,15 +7,19 @@ class fcb_apache_v2::windows::config {
   $config_file  = $fcb_apache_v2::windows::config_file
   $vhost        = $fcb_apache_v2::windows::vhost
 
+  Concat::Fragment [
+    notify => Exec[ 'Restart Apache' ],
+  ]
+
   concat { "${$install_path}/${$apche_dir}/conf/httpd.conf":
     ensure => present,
+    notify => Exec[ 'Restart Apache' ],
   }
 
   concat::fragment { 'httpd.conf':
     target  => "${$install_path}/${$apche_dir}/conf/httpd.conf",
     content => template("${module_name}/windows_httpd.conf.erb"),
     order   => '01',
-    notify  => Exec[ 'Restart Apache' ],
   }
 
   concat::fragment { 'vhost':
