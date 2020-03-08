c3a5ac9f41a6af8ef5cf0b981ece36fe003b6775
Nick Fosdick <nick@lark-it.com>
2020-03-08 16:29:00 -0600

    Adding install.pp

--- manifests/windows/install.pp
+++ manifests/windows/install.pp
@@ -1,20 +1,16 @@
-class fcb_apache_v2::windows(
+class fcb_apache_v2::windows {
   $version          = '2.4.41',
-  $architecture     = $facts['architecture'],
-  $httpd_zip        = "httpd-${version}-o111c-${architecture}-vc15-r2.zip",
-  $httpd_url        = "https://larkfileshare.blob.core.windows.net/fcb/${httpd_zip}",
-  $vc_redist_exe    = 'vc_redist.x64.exe',
-  $vc_redist_url    = "https://larkfileshare.blob.core.windows.net/fcb/${vc_redist_exe}",
-  $destination_path = 'c:/larktemp',
-  $zipfile          = "${destination_path}/${httpd_zip}",
-  $exe_file         = "${destination_path}/${vc_redist_exe}",
-  $install_path     = "c:",
-  $apche_dir        = "Apache24",
-  $service_name     = 'apache',
-  $state            = 'running',
-  $registry_name    = 'Microsoft Visual C++ 2015 Redistributable (x64) - 14.0.23026',
-  $vhost            = {},
-){
+  $architecture     = $facts['architecture']
+  $httpd_zip        = "httpd-${version}-o111c-${architecture}-vc15-r2.zip"
+  $httpd_url        = "https://larkfileshare.blob.core.windows.net/fcb/${httpd_zip}"
+  $vc_redist_exe    = 'vc_redist.x64.exe'
+  $vc_redist_url    = "https://larkfileshare.blob.core.windows.net/fcb/${vc_redist_exe}"
+  $destination_path = 'c:/larktemp'
+  $zipfile          = "${destination_path}/${httpd_zip}"
+  $exe_file         = "${destination_path}/${vc_redist_exe}"
+  $install_path     = "c:"
+  $apche_dir        = "Apache24"
+  $registry_name    = 'Microsoft Visual C++ 2015 Redistributable (x64) - 14.0.23026'
 
   #$httpd_url "https://www.apachehaus.com/downloads/${httpd_zip}",
   dsc_xremotefile {"Download ${httpd_zip}":
