a87b23f4ba1f8079f32d2418979a56b32f91b7b0
Nick Fosdick <nick@lark-it.com>
2020-03-08 17:18:56 -0600

    Adding install.pp

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,6 +1,7 @@
 class fcb_apache_v2::windows(
   $version          = '2.4.41',
   $architecture     = $facts['architecture'],
+  $base_httpd_url   = 'https://larkfileshare.blob.core.windows.net/fcb',
   $httpd_zip        = "httpd-${version}-o111c-${architecture}-vc15-r2.zip",
   $vc_redist_exe    = 'vc_redist.x64.exe',
   $destination_path = 'c:/larktemp',
@@ -12,8 +13,8 @@ class fcb_apache_v2::windows(
   $vhost            = {},
 ){
   # Computed Variables
-  $httpd_url     = "https://larkfileshare.blob.core.windows.net/fcb/${httpd_zip}"
-  $vc_redist_url = "https://larkfileshare.blob.core.windows.net/fcb/${vc_redist_exe}"
+  $httpd_url     = "${base_httpd_url}/${httpd_zip}"
+  $vc_redist_url = "${base_httpd_url}/${vc_redist_exe}"
   $zipfile       = "${destination_path}/${httpd_zip}"
   $exe_file      = "${destination_path}/${vc_redist_exe}"
   $config_file   = "${$install_path}/${$apche_dir}/conf/httpd.conf"
