ca03b1ef76c9e761b710f5caf5eee0a36f3125af
Nick Fosdick <nick@lark-it.com>
2020-03-09 20:14:33 -0600

    test

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -36,6 +36,7 @@ class fcb_apache_v2::windows::config {
   $vhosts.each |$vhost, $config| {
 #    $vhosts_merged = $vhost_defaults['defaults']['default'] + $vhosts[$vhost]
     $vhosts_merged = $vhost_defaults['defaults'] + $config
+    $directory_merged=$config['Directory']
     notify{"Nick $vhosts_merged":} 
   #  concat::fragment { 'vhost':
   #    target  => "${$install_path}/${$apache_dir}/conf/httpd.conf",
