57b5a42111b3f1380f39672d6c629665683c3b74
Nick Fosdick <nick@lark-it.com>
2020-03-09 20:38:17 -0600

    test

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -39,10 +39,10 @@ class fcb_apache_v2::windows::config {
 
 #    }
     notify{"Nick $vhosts_merged":} 
-  #  concat::fragment { 'vhost':
-  #    target  => "${$install_path}/${$apache_dir}/conf/httpd.conf",
-  #    content => template("${module_name}/windows_vhost.erb"),
-  #    order   => '10',
-  #  }
+    concat::fragment { 'vhost':
+      target  => "${$install_path}/${$apache_dir}/conf/httpd.conf",
+      content => template("${module_name}/windows_vhost_new.erb"),
+      order   => '10',
+    }
   }
 }
